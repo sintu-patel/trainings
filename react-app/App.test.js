a56ff37f9f58329c32e0286c7b0c1b8c55fedf91
ssingh172 <ssingh172@sapient.com>
2018-02-23 13:05:03 +0530

    Add unit test

--- react-app/App.test.js
+++ react-app/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import App from './App.jsx';
 
 describe('App', () => {
+
+	test('Snapshot is equal to previous snapshot', () => {
+        const tree = renderer
+            .create(<App />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
     
     const wrapper = mount(<App />);
     test('App Renders', () => {
